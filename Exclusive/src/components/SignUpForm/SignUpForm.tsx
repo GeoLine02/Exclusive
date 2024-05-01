c9653b1ad8a3a98eb4bee26c735411d2c7322df5
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-05-01 11:24:06 +0400

    refactor: sign up form

--- Exclusive/src/components/SignUpForm/SignUpForm.tsx
+++ Exclusive/src/components/SignUpForm/SignUpForm.tsx
@@ -1,93 +1,63 @@
 import AuthSideImage from "../../assets/Auth-side-image.svg";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import routes from "../../constants/routes";
-import { useState } from "react";
-import { UserType } from "../../types";
-import { signUp } from "../../api/atuh";
 import { ClipLoader } from "react-spinners";
-const SignUpForm = () => {
-  const [user, setUser] = useState<UserType>({
-    email: "",
-    userName: "",
-    password: "",
-  });
-  const [loader, setLoader] = useState<boolean>(false);
-  const navigate = useNavigate();
-  const handleOnChange = (fieldName: keyof UserType, value: string) => {
-    setUser((prev) => ({
-      ...prev,
-      [fieldName]: value,
-    }));
-  };
+import Form from "../ui/Form";
+import Input from "../ui/Input";
+import Button from "../ui/Button";
+import { UserType } from "../../types/user";
+import SlowServerWarning from "../warnings/SlowServerWarning";
 
-  const handleSignUp = (e: React.FormEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setLoader(true);
-    signUp(user)
-      .then(() => {
-        navigate(routes.signIn);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => {
-        setLoader(false);
-      });
-  };
+
+interface SignUpPropsType {
+  handleSetUser: (fieldName: keyof UserType, value: string) => void;
+  handleSignUp: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
+  error: string;
+  isLoading: boolean;
+}
+
+const SignUpForm = ({error, handleSetUser, handleSignUp, isLoading} : SignUpPropsType) => {
+
   return (
-    <div className="flex justify-center items-center md:flex  md:gap-10">
+      <div className="flex justify-center items-center md:flex md:gap-10">
       <img
         className="hidden md:block max-h-[600px]"
         src={AuthSideImage}
         alt="auth side image"
       />
-      <form className="max-w-96 flex flex-col justify-center">
-        <div className="flex flex-col gap-3">
+    <Form onSubmit={handleSignUp} className="max-w-80" align="vertical">
+        
           <h1 className="text-4xl font-medium">Create an account</h1>
-          <p className="font-medium">Enter your details below</p>
-        </div>
+        
         <div className="flex flex-col gap-5 mt-8">
-          <div className="border-b-2 border-gray-400">
-            <input
-              onChange={(e) => handleOnChange("userName", e.target.value)}
-              className="outline-none bg-transparent "
-              type="text"
-              placeholder="Username"
-            />
-          </div>
-          <div className="border-b-2 border-gray-400">
-            <input
-              onChange={(e) => handleOnChange("email", e.target.value)}
-              className="outline-none bg-transparent "
-              type="email"
-              placeholder="Email"
-            />
-          </div>
-          <div className="border-b-2 border-gray-400 w-full">
-            <input
-              onChange={(e) => handleOnChange("password", e.target.value)}
-              className=" outline-none bg-transparent "
-              type="passowrd"
-              placeholder="Password"
-            />
-          </div>
+          <Input name="userName" backgroundColor="none" underlined placeholder="Username" type="text" handleChange={(e) => handleSetUser("userName", e.target.value)} />     
+          <Input name="email" backgroundColor="none" underlined placeholder="Email" type="text" handleChange={(e) => handleSetUser("email", e.target.value)} />     
+          <Input name="password" backgroundColor="none" underlined placeholder="Passwrod" type="password" handleChange={(e) => handleSetUser("password", e.target.value)} />
         </div>
-        <div className=" w-full flex justify-center items-center mt-5 gap-6">
-          <button
-            onClick={handleSignUp}
-            className="h-[56px] w-full bg-[#DB4444] rounded-md text-white"
-          >
-            Create Account
-          </button>
-        </div>
+        <Button type="submit" className="mt-6" align="horizontal" background="red" textColor="light">
+          Sign Up
+        </Button>
         <p className="mt-4">
           Already have account?&nbsp;&nbsp;
-          <Link to={routes.signIn} className="border-b-2 border-gray-400">
+          <Link to={routes.signIn} className="border-b-[1px] border-gray-300">
             Log in
           </Link>
         </p>
-      </form>
-      {loader && <ClipLoader color="#DB4444" />}
+        <div>
+          {isLoading && 
+            <div className=" w-full flex justify-center items-center mt-10">
+          <ClipLoader size={30} color="#DB4444" />
+          <SlowServerWarning isLoading={isLoading} />
+            </div>
+          }
+          {error &&
+           <p>
+              {error}
+           </p>
+           }  
+        </div>
+              
+    </Form>
     </div>
   );
 };
