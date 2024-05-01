b5fc95ebf57d13109085efde9e930e15a4073035
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-05-01 22:56:35 +0400

    remove: Slider component from Home page

--- Exclusive/src/Pages/Home/Home.tsx
+++ Exclusive/src/Pages/Home/Home.tsx
@@ -1,14 +1,10 @@
 import BestSellings from "../../components/BestSellings/BestSellings";
 import FreshSales from "../../components/FreshSales/FreshSales";
 import NewArrival from "../../components/NewArrival/NewArrival";
-import Slider from "../../components/Slider/Slider";
 
 const Home = () => {
   return (
     <div>
-      <div className="grid items-center justify-center">
-        <Slider />
-      </div>
       <div>
         <FreshSales />
       </div>
