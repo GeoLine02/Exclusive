94ad33fa350ca683d0fe7c3138855aed48cde317
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-05-02 16:00:19 +0400

    refactor: BestSellings component

--- Exclusive/src/components/BestSellings/BestSellings.tsx
+++ Exclusive/src/components/BestSellings/BestSellings.tsx
@@ -1,49 +1,43 @@
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../../store/store";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store/store";
 import { useEffect } from "react";
 import { fetchBestSellings } from "../../features/productSlice/productSlice";
-import DiscountedItem from "../FreshSales/DiscountedItem";
 import { useNavigate } from "react-router-dom";
 import routes from "../../constants/routes";
+import BestSellingsList from "../blocks/bestSellingsList/BestSellingsList";
+import Button from "../ui/Button";
 
 const BestSellings = () => {
-  const bestSellings = useSelector(
-    (state: RootState) => state.produts.bestSellings
-  );
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
   useEffect(() => {
     dispatch(fetchBestSellings());
   }, [dispatch]);
+
+  const handleNavigate = () => {
+    navigate(routes.allProducts);
+  };
   return (
-    <div className="py-4 px-6">
+    <div className="py-4 px-6 max-w-screen-xl w-full mx-auto">
       <div className="flex items-center gap-2">
         <span className="bg-[#DB4444] h-8 w-4 rounded-md"></span>
         <span className="text-[#DB4444] font-bold">This month</span>
       </div>
-      <div className="flex justify-between">
+      <div className="flex justify-between items-center">
         <h1 className="text-3xl font-medium">Best Selling Products</h1>
-        <button
-          onClick={() => {
-            navigate(routes.allProducts);
-          }}
-          className="text-white font-medium bg-[#DB4444] rounded-sm px-6 py-2"
-        >
-          View All
-        </button>
+        <div className="max-w-56">
+          <Button
+            onClick={handleNavigate}
+            background="red"
+            textColor="light"
+            type="button"
+            align="default"
+          >
+            View All
+          </Button>
+        </div>
       </div>
-      <div className="mt-6 flex gap-4 whitespace-nowrap overflow-x-auto md:grid md:grid-cols-4">
-        {bestSellings?.products.map((item) => (
-          <DiscountedItem
-            id={item.id}
-            title={item.title}
-            images={item.images}
-            discountPercentage={item.discountPercentage}
-            price={item.price}
-            key={item.title}
-          />
-        ))}
-      </div>
+      <BestSellingsList />
     </div>
   );
 };
